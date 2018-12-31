ee18fa3c92468d49b75e5b0f6daca079fd3d394a
Daniel Weinman <namniewd@gmail.com>
2018-12-31 11:47:49 -0500

    added footer with ability to increase counter on likes and retweets

--- src/App.js
+++ src/App.js
@@ -2,14 +2,43 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js'
 import CardContainer from './components/CardComponents/CardContainer.js'
+import Footer from './components/FooterComponents/Footer.js'
 
-const App = () => {
-  return (
-    <div className="app">
-      <HeaderContainer />
-      <CardContainer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      retweets: 0
+    }
+  }
+
+  handleLike = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  };
+
+  handleRetweet = () => {
+    this.setState({
+      retweets: this.state.retweets + 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <HeaderContainer />
+        <CardContainer />
+        <Footer 
+          handleLike={this.handleLike} 
+          likes={this.state.likes}
+          handleRetweet={this.handleRetweet}
+          retweets={this.state.retweets}
+        />
+      </div>
+    );
+  }
 };
 
 export default App;
--- src/components/FooterComponents/Footer.js
+++ src/components/FooterComponents/Footer.js
@@ -4,7 +4,12 @@ import './Footer.css';
 const Footer = props => {
     return (
         <footer className='footer'>
-            
+            <i className="far fa-comment"></i>
+            <i className="fas fa-retweet" onClick={props.handleRetweet}> {props.retweets}</i>
+            <i className="far fa-heart" onClick={props.handleLike}> {props.likes}</i>
+            <i className="far fa-envelope"></i>
         </footer>
     )
 }
+
+export default Footer;
