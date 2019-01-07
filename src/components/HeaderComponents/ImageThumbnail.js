4cdb81c6841eec71b1db4efec4d1b9fff3c6b38a
Daniel Weinman <namniewd@gmail.com>
2019-01-08 01:09:27 +0530

    made header pixel perfect

--- src/components/HeaderComponents/ImageThumbnail.js
+++ src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = props => {
-    return <img className="header-image" src="images/lambda-logo.jpg" alt="lambda-logo" />
+    return <img className="header-image" src="../../../images/lambda-logo.jpg" alt="lambda-logo" />
 };
 
 export default ImageThumbnail;
